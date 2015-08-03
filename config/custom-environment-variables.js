4551af808a31479c8a2dc074b31de0e81e167167
Pawan Sasanka <pawan@duosoftware.com>
2015-08-03 11:27:40 +0530

    Updated

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "DB": {
+        "Type":"SYS_DATABASE_TYPE",
+        "User":"SYS_DATABASE_POSTGRES_USER",
+        "Password":"SYS_DATABASE_POSTGRES_PASSWORD",
+        "Port":"SYS_SQL_PORT",
+        "Host":"SYS_DATABASE_HOST",
+        "Database":"SYS_DATABASE_POSTGRES_USER"
+    },
+
+
+    "Redis":
+    {
+        "ip": "SYS_REDIS_HOST",
+        "port": "SYS_REDIS_PORT"
+
+    },
+
+    "Host":
+    {
+        "domain": "HOST_NAME",
+        "port": "HOST_APPREGISTRY_PORT",
+        "version": "HOST_VERSION"
+    }
+};
+
+//NODE_CONFIG_DIR
